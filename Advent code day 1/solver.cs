05e45a7972a3ad16ea6711875780dc86498d4ab4
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2023-12-02 17:05:10 +0200

    Add files via upload

---
+++ Advent code day 1/solver.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Linq;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace AdventOfCodeDay01
+{
+   class Program{
+
+
+static string[] input = File.ReadAllLines("input.txt");
+static int selectedLine = 0;
+
+static List<int> CalibrationValues = new List<int>();          
+public static void Main(string[] args)
+
+{
+  for(int i = 0; i < input.Length; i++)
+  {
+     selectedLine = i;
+    Console.WriteLine(solve());
+    CalibrationValues.Add(solve());
+      
+         
+
+
+  }
+  
+ int solution = CalibrationValues.Sum(); 
+Console.WriteLine(solution);
+Console.ReadLine();
+
+}
+
+public static int solve()
+{
+string workingString = input[selectedLine];
+Regex regex = new Regex(@"\d|one|two|three|four|five|six|seven|eight|nine", RegexOptions.IgnoreCase);
+Regex regex2 = new Regex(@"\d|one|two|three|four|five|six|seven|eight|nine", RegexOptions.RightToLeft);
+Match match = regex.Match(workingString);
+Match match2 = regex2.Match(workingString);
+string first = match.Value;
+string last = match2.Value;
+return(parseMatch(first) * 10 + parseMatch(last));
+
+
+}
+
+static int parseMatch(string st) => st switch{
+
+"one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        var d => int.Parse(d)
+};
+
+   }
+
+
+
+}

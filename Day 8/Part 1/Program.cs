71bc2ea25f146bab4e95c4beebe91bf4c3a14b11
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2023-12-08 20:54:23 +0200

    Add files for day 8

---
+++ Day 8/Part 1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static string[] input = File.ReadAllLines(@"C:\Users\Mitko\Projects\C#\Advent of code\Day 8\Part 1\bin\Debug\net7.0\input.txt");
+        static void Main(string[] args)
+        {
+            Program solution = new Program();
+            char[] instructions = input[0].ToCharArray();
+            int i = 0;
+            int currentLine = 2;
+            int steps = 0;
+            bool zNotFound = true;  
+
+            while(zNotFound == true)
+            {
+                int direction = 0;
+                if(i == instructions.Length)
+                {
+                    i = 0;
+                }
+                switch(instructions[i])
+                {
+                    case 'L':
+                    direction = 0;
+                    break;
+
+                    case 'R':
+                    direction = 1;
+                    break;
+                }
+                
+                currentLine = solution.calculatePath(currentLine, direction);
+                i++;
+                steps++;
+                if(currentLine == -1)
+                {
+                    zNotFound = false;
+                }
+
+            }
+            Console.WriteLine(steps);
+            Console.ReadLine();
+        }
+
+        int calculatePath(int arrayNum, int direction)
+        {
+            string leftPath = input[arrayNum].Substring(input[arrayNum].IndexOf("(") + 1, 3);
+            string rightPath = input[arrayNum].Substring(input[arrayNum].IndexOf(",") + 2, 3);
+            string searchingFor = "e";
+            
+            switch(direction)
+            {
+                case 0:
+                searchingFor = leftPath;
+                break;
+
+                case 1:
+                searchingFor = rightPath;
+                break;
+            }
+
+            foreach(string s in input)
+            {
+                try
+                {                
+                  if (s.Substring(0, 3) == searchingFor)
+                  {
+                      if(s.Substring(0, 3) == "ZZZ")
+                      {
+                          return -1;
+                      }
+                      return Array.IndexOf(input, s);
+                                   
+                  }
+                }
+               catch
+               {
+                  continue;  
+               }
+            }
+            return 2;
+        }
+    }
+}
